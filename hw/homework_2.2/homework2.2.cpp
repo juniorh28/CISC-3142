d7cd05af547ae18738ee48b60ba1a07e4065fc1f
juniorh28 <juniorh2897@gmail.com>
2021-10-20 21:31:17 -0400

    not perfect but syntaxfully correct

--- hw/homework_2.2/homework2.2.cpp
+++ hw/homework_2.2/homework2.2.cpp
@@ -10,23 +10,33 @@ using namespace std;
 enum Gender{female, male};
 
 class TTPlayer{
-    const* name;
+    char* name;
     Gender gen;
 
 public:
+
     //allocate memory for TTPlayer to copy the arguements
     //this will be used by the regular constructor outside class
     TTPlayer(const char *s, Gender g);
 
     // copy constructor
     //inline inmplementation
-    TTPlayer(const TTPlayer &ttp) : TTPlayer{const* name, Gender gen;}{};
+    TTPlayer(const TTPlayer &ttp) : TTPlayer(ttp.name, ttp.gen){};
 
     //TTPlayer's enum will let us know which to look for for gen.
     std::string toString()
     {
-        return name + ", " + (gen ? "M" : "F");
+        return name + std::string(", ") + (gen ? "M" : "F");
     }
+
+    //bool operator is going to compare TTPlayer's own gender with the rhs's gender
+    //return true if its less than and false if not.
+    bool operator<(const TTPlayer& rhs){
+        return this->gen < rhs.gen;
+    }
+
+    //release resources
+    ~TTPlayer(){delete[] name;};
 
     //strLength is needed to copy the name from char* s
     int strLen(const char *s)
@@ -38,34 +48,25 @@ public:
         }
         return i;
     };
-
-    //bool operator is going to compare TTPlayer's own gender with the rhs's gender
-    //return true if its less than and false if not.
-    bool operator<(const TTPlayer &rhs){
-        return this->gen < rhs.gen;
-    }
-
-    //release resources
-    ~TTPlayer(){delete[] name;};
-}
+};
 
 //Not inline mean outside of class
 //regular constructor (NOT inline)
 //Since gen does not require any manipulation to gain access, we include it inline initalizer list
 //since since name needs to be copied over from the pointer, we need a for loop and strLength function.
-TTPlayer::TTPlayer(const char *s, Gender g): gen(g)
-{
+//defining not declaring outside a class
+TTPlayer::TTPlayer(const char *s, Gender g): gen(g){
     //copy name into ttp class
     int length = strLen(s);
     name = new char [length + 1];
-    for(int i =0; i<length; ++i){
+    for(int i =0; i < length; ++i){
         name[i] = s[i]; 
     }
-
 };
 
 int main(){
     try{
+
         TTPlayer players[] = {
             {"Player1", Gender::male},
             {"Player2", Gender::female},
@@ -77,14 +78,10 @@ int main(){
 
         
         for(int i=0; i < n; ++i){
-            TTPlayer ttp1(players[i].name, players[i].gen);
              for(int j=i+1; j < n; ++j){
-                TTPlayer ttp2(players[j].name, players[j].gen);
-                if (ttp1 == tt2)
-                    continue;
-                else{
-                    ttp1.toString();
-                }
+                Ordpair<TTPlayer> pair(players[i], players[j]);
+                pair.toString();
+
             }
         }
          
@@ -92,5 +89,6 @@ int main(){
     }
     catch(std::invalid_argument const &ex)
     {
+        cout << "not valid pair" << endl;
     }
-}
+};
--- hw/homework_2.2/ordpair.hpp
+++ hw/homework_2.2/ordpair.hpp
@@ -3,36 +3,53 @@
 //Homework 2.2 .hpp
 
 #include <iostream>
+#include <stdexcept>
 
 using namespace std;
 
 template <typename T>
-class Ordpair{
+class Ordpair
+{
 
 private:
     T first;
     T second;
 
 public:
-    Ordpair(T first, T second);
-    
-    //since first and second will be defined in TTPlayer with the enum values, we will be able to tell if f<s and throw on true;
-    if(first < second){
-        throw std::invalid_argument;
-    }
-    void setFirst(T first){this->first = first;};
-    void setSecond(T second){this->second = second;};
-    T getFirst(){return first;};
-    T getSecond(){return second;};
-    
+    Ordpair(T f, T s) : first{f}, second{s}{ 
+        //since first and second will be defined in TTPlayer with the enum values, we will be able to tell if f<s and throw on true;
+        if (this->first < this->second)
+        {
+            throw invalid_argument("Error");
+        }
+
+    };
+
+    void setFirst(T first)
+    {
+        if (first == 1)
+            this->first = first;
+        else
+            throw invalid_argument("Error");
+    };
+    void setSecond(T second)
+    {
+        if (second == 0)
+            this->second = second;
+        else
+            throw invalid_argument("Error");
+    };
+    T getFirst() { return first; };
+    T getSecond() { return second; };
+
     //using type T, take in 'a' ordpair's data member
     //if the 'a' ordpair data member is the same as this ordpair's data member, return true;
-    const T &operator==(Ordpair<T> a) const{ 
-        if(this->first == a.first && this->second == a.second)
-            return true; 
+    const T &operator==(Ordpair<T> a) const
+    {
+        if (this->first == a.first && this->second == a.second)
+            return true;
     };
-    
-    string toString() { return "(" + this->first + " | " + this->second + ")" + endl; };
 
-    ~Ordpair() { delete[] first, second;};
-}
+    std::string toString() {return "(" + first.toString() + " | " + second.toString() + ")"; };
+
+};
